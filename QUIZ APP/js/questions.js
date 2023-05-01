f04e8e5adddd45bd62c34ce236b8b4de5e2698e1
Sonali553 <83964942+Sonali553@users.noreply.github.com>
2023-05-01 20:09:05 +0530

    Add files via upload

---
+++ QUIZ APP/js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many operators are in C language?",
+    answer: "32",
+    options: [
+      "45",
+      "32",
+      "96",
+      "65"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the output of the program? if int a = 1; printf(\"%d\", a++);",
+    
+    answer: "1",
+    options: [
+      "0",
+      "1",
+      "undetermined;",
+      "2;"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the output of the program? if int a = 1; printf(\"%d\", --a);",
+    answer: "0",
+    options: [
+      "1",
+      "-1",
+      "0",
+      "error"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the output of the program? if int a = 1; printf(\"%d\", (--a) + (++a));",
+    answer: "2",
+    options: [
+      "0",
+      "3",
+      "2",
+      "error"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the output of the program? if int a = 1; printf(\"%d\", (a += 1) + (++a));",
+    answer: "6",
+    options: [
+      "5",
+      "Compilation error",
+      "6",
+      "7"
+    ]
+  },
+];
